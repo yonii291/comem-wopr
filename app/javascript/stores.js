aec0b4ed4cc85393642096a10182edf8eac9d613
Simon Oulevay <simon.oulevay@gmail.com>
2020-01-13 16:56:09 +0100

    Split components

--- app/javascript/stores.js
+++ app/javascript/stores.js
@@ -1,3 +1,28 @@
 import { writable } from 'svelte/store';
 
-export const currentGame = writable(Array(9).fill(null));
+  import { uuidv4 } from './utils';
+
+function createGameStore() {
+	const { set, subscribe, update } = writable(0);
+
+	return {
+    subscribe,
+
+		start: ai => set({
+      ai,
+      board: Array(9).fill(null),
+      finished: false,
+      id: uuidv4()
+    }),
+
+    play: (cell, value) => update(g => {
+      const board = g.board.slice();
+      board[cell] = value;
+      return { ...g, board };
+    }),
+
+		finish: () => update(g => ({ ...g, finished: true }))
+	};
+}
+
+export const currentGame = createGameStore();
