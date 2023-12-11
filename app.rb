38b02746ed82a8b311e0102ea3921019895e9dcf
Simon Oulevay <simon.oulevay@gmail.com>
2023-12-11 10:39:36 +0100

    Fix production mode

--- app.rb
+++ app.rb
@@ -9,8 +9,12 @@ require 'sinatra/reloader' if WOPR.development?
 WOPR.ensure_database_connection!
 WOPR.logger.info Paint["WOPR is running in #{WOPR.env} mode", :green]
 
+# Set the rack environment so it is picked up by puma.
+ENV['RACK_ENV'] = WOPR.env unless ENV['RACK_ENV']
+
 set :bind, '0.0.0.0'
 set :port, WOPR.port
+set :environment, WOPR.env
 
 get '/' do
   slim :index, locals: { bundle: WOPR.javascript_bundle }
